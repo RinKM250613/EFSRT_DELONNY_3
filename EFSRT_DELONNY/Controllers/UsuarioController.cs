e35b9d7e9ea0972c18b5034c0c28eb9e5358168e
RinKM250613 <I202407573@cibertec.edu.pe>
2025-06-13 19:01:07 -0500

    oe sair date de baja

---
+++ EFSRT_DELONNY/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFSRT_DELONNY.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
